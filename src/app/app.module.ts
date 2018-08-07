c704d59051ad0c74e59f4e158fd5544a3251dc69
lballabe <lballabe@cisco.com>
2018-08-06 22:49:34 -0400

    added hero & villain components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { VillainsComponent } from './villains/villains.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    VillainsComponent
   ],
   imports: [
     BrowserModule
