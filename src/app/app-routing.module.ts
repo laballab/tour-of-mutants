4d56e2bb5feb1e16cd20882b281402cc2f7602c4
lballabe <lballabe@cisco.com>
2018-08-11 14:07:36 -0400

    added detail view to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { VillainsComponent } from './villains/villains.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { VillainDetailComponent } from './villain-detail/villain-detail.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'villains', component: VillainsComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes/detail/:id', component: HeroDetailComponent },
+  { path: 'villains/detail/:id', component: VillainDetailComponent },
 ];
 
 @NgModule({
